5e13f9a330a86cc1131f09b98b0ebd8e26cc8b3d
Bereket Keleta <bereket.keleta20@gmail.com>
2022-06-02 06:36:39 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * return : always 0 (success)
+ * This program shows how we can display 
+ * a string using puts function
  */
-/* print a string using puts function*/
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
