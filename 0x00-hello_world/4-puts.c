6f49557290d874277ccbeefef8843ddcc87cd93d
Bereket Keleta <bereket.keleta20@gmail.com>
2022-06-02 06:30:31 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,10 @@
  *
  * return : always 0 (success)
  */
+/* print a string using puts function*/
 int main(void)
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0); /*return success*/
+	return (0); 
 }
