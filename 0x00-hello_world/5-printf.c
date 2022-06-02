54b229417d096998bbf05b5f86a94a7648cb819e
Bereket Keleta <bereket.keleta20@gmail.com>
2022-06-02 06:51:51 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,8 @@
  * shows how to display a string 
  * using printf function
  */
-int main(void){
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
