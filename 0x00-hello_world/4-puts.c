9368fd3f253b36ffb6e85a29083f2e40eab6d0f1
Bereket Keleta <bereket.keleta20@gmail.com>
2022-06-02 13:35:08 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
  * This program shows how we can display 
  * a string using puts function
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	/*displays a string using put function*/
+	puts("\"Programming is like building a multilingual puzzle");
+	/* exit program with success*/
+	return (0);
 }
