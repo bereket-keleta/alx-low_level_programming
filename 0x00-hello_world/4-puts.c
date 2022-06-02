aeb583452d5d35de4689780972041317ad160ea8
Bereket Keleta <bereket.keleta20@gmail.com>
2022-06-02 06:33:13 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  * return : always 0 (success)
  */
 /* print a string using puts function*/
+
 int main(void)
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0); 
+	return (0);
 }
