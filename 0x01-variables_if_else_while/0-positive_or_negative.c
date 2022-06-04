54d163b0f494eb736c8b6521c16262ee88f687f1
Bereket Keleta <bereket.keleta20@gmail.com>
2022-06-04 03:27:39 -0700

    a program print whether the numberis positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+
 /**
- * the program asigns a random number to the variable 
- * and determins wheather the number is +ve or -ve
+ * main - assign a random number to the variable num and then
+ * determines wheather the number is positive, negative or zero
+ * Return: 0
  */
 
 int main(void)
 {
-	int n;
+	int num;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if(n > 0){
-	printf("%d is positive\n",n);
-	}
-	else if(n < 0)
+	num = rand() - RAND_MAX / 2;
+	if (num > 0)
 	{
-	printf("%d is negative",n);
+	printf("%d is positive\n", num);
+	}
+	else if (num < 0)
+	{
+	printf("%d is negative\n", num);
 	}
 	else
 	{
-	printf("%d is zero",n);
+	printf("%d is zero\n", num);
 	}
 	return (0);
 }
