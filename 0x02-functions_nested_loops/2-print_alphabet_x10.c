83833bf30cb97d1939724890abb47994cd360f2d
Bereket Keleta <bereket.keleta20@gmail.com>
2022-06-08 03:54:24 -0700

    prints 10 times the alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * void print_alphabet_x10 - prints 10x the alphabet
+ *
+ * Return : 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int n, counter;
+
+	counter = 0;
+	while (counter < 10)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+		counter++;
+		_putchar('\n');
+	}
+}
