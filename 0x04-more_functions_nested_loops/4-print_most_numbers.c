6d5a30590835284f1d7436d8f2ed8f582726b998
Bereket Keleta <bereket.keleta20@gmail.com>
2022-06-09 12:21:00 -0700

    print nums 0-9

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Print the numbers
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int x = 0;
+
+	for (; x <= 9; x++)
+	{
+		if (x == 2 || x == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(x + '0');
+		}
+	}
+	_putchar('\n');
+}
