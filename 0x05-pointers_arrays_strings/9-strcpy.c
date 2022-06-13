3936623ab8bb7951bb0c51b91495f52383edb451
Bereket Keleta <bereket.keleta20@gmail.com>
2022-06-13 15:28:23 -0700

    copies the string pointed to by src

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,11 +12,14 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
-
-	for (; src[i] != '\0'; i++)
+	int i;
+	for (i = 0; *src != '\0'; i++)
 	{
-		dest[i] = src[i];
+		dest[i] = *src;
+		src++;
 	}
+
+	dest[i++] = *src;
+
 	return (dest);
 }
